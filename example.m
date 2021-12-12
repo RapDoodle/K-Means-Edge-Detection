6ead6e8f178af086f2ce6aac1cf2a4987dcdcc2d
Bowen WU <bowenbhwu@gmail.com>
2021-12-12 17:23:23 +0800

    Added: Usage of the algorithms

---
+++ example.m
@@ -0,0 +1,29 @@
+%% Introduction
+% This script contains examples of how to invoke the routines
+% Authors:
+%   Bohui WU, Rui LIU
+
+%% Load the images
+% The image will be loaded as an grayscale image and resized to 512*512
+img = loadImg('05.jpg', [512, 512], true);
+% Show the loaded image
+imshow(img);
+
+%% Perform image segmentation (optional)
+% This is just part of our assignment. Feel free to ignore ths section.
+% It has nothing to do with image segmentation
+% Perform image segmentation
+k = 16;
+imgSeg = imageSegmentation(img, k);
+
+% Show the segmented image
+figure
+imshow(imgOut);
+title('k = ' + string(k));
+
+%% Perform Edge Detection
+% Perform edge detection
+mask = edgeDetection(img);
+
+% Plot the original image along with the mask
+imgMergePlot(img, ones(size(img)).*mask);
