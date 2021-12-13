42eba4638886779578d67200ae0e6fc232416e37
Bowen <bowenbhwu@gmail.com>
2021-12-14 04:42:50 +0800

    Updated: Edge identification

--- edgeDetection.m
+++ edgeDetection.m
@@ -3,8 +3,8 @@ function mask = edgeDetection(img, minDim, stepSize, iter, opt)
 % Arguments:
 %   img: A grayscale image. Should contain the intensity values of the
 %       image.
-%   minDim: The minimum dimension of the standard deviation calculation 
-%       filter.
+%   minDim: The minimum dimension of the window used to calculation the 
+%       standard deviation window.
 %   stepSize: The size of each step. Determines the maxDim, which is
 %       minDim + stepSize*(iter-1)
 %   iter: The number of iterations. Determines the maxDim. See stepSize.
@@ -20,13 +20,13 @@ if isempty(gcp('nocreate'))
 end
 
 % Handle default values
-if nargin < 2 || ~isempty(minDim)
-    minDim = 3;
+if nargin < 2 || isempty(minDim)
+    minDim = 7;
 end
-if nargin < 3 || ~isempty(stepSize)
+if nargin < 3 || isempty(stepSize)
     stepSize = 4;
 end
-if nargin < 4 || ~isempty(iter)
+if nargin < 4 || isempty(iter)
     iter = 8;
 end
 if nargin < 5
@@ -41,17 +41,14 @@ end
 if ~isfield(opt, 'showMaskProd')
     opt.showMaskProd = false;
 end
-if ~isfield(opt, 'p')
-    opt.p = 0.2;
-end
 
 % Calculate the values to be used in subsequent calculations
 [m, n] = size(img);
 maxDim = minDim + stepSize*(iter-1);
-numSteps = (maxDim-minDim)/stepSize;
+numSteps = max(((maxDim-minDim)/stepSize), 1);
 
 % Used to record the masks generated on each iteration
-% The first mask in the cell is the one calculated with the largest filter
+% The first mask in the cell is the one calculated with the largest window
 % size.
 masks = cell(numSteps, 1);
 
@@ -89,7 +86,7 @@ parfor i=1:numSteps
     
     % *Naively* assume the number of pixels in the region for edges is 
     % less than the number of pixels in the background region.
-    if length(find(asOr == 1)) < length(find(asOr == 2))
+    if res.centroids(1) > res.centroids(2)
         bgCluster = 2;
     else
         bgCluster = 1;
@@ -126,53 +123,9 @@ if opt.showMaskProd
     end
 end
 
-% The following for-loop solves the problem that when the filter size 
-% is large, images containing a lot of edges may incorrectly identify 
-% the backgrounds and edges: the naive assumption above may guide the 
-% algorithm to identify the edges as background when the number of pixels 
-% for edges is more than that for the actual backgrounds. 
-% 
-% The following for-loop assumes that there are fewer edges than 
-% backgrounds in the smallest filter. It iteratively checks whether there 
-% is a flip between the i-th filter and (i-1)-th mask.
-% 
-% A “flip” means most of the pixels that classified as background in the 
-% i-th mask became the edges in the (i-1)-th mask. This phenomenon can be 
-% indicated by checking the element-wise multiplication of the two masks. 
-% If the resulting mask contains drastically less edges (1s), then we can 
-% determine there is a “flip.” In such cases, the algorithm flips all the 
-% values in the (i-1)-th mask, setting all ones to zeros and all zeros to 
-% ones.
-% At the same time, compute the final mask: the element-wise multiplication
-% of all masks.
-mask = masks{numSteps};
-for i=numSteps:-1:2
-    % Check the i-th and (i-1)-th mask
-    maskProd = masks{i} .* masks{i-1};
-    
-    % Count the number of edges (pixels) in the (i-1)-th mask
-    cmn = length(find(masks{i-1} == 1));
-    
-    % Count the number of edges after element-wise multiplication
-    cmo = length(find(maskProd == 1));
-    
-    if cmn * opt.p > cmo
-        masks{i-1} = abs(masks{i-1} - 1);
-    end
-    
-    % Update the final mask
-    mask = mask .* maskProd;
-    
-    % Debug: Show the element-wise multiplication between the two adjacent 
-    % masks
-    if opt.showMaskProd
-        dim = maxDim - stepSize*(i-1);
-        dim2 = maxDim - stepSize*(i-2);
-        figure
-        imshow(masks{i});
-        title(sprintf("Mask product between dimension %d*%d and %d*%d", ...
-            dim, dim, dim2, dim2));
-    end
+mask = ones(m, n);
+for i=1:numSteps
+    mask = mask .* masks{i};
 end
 
 % Debug tool: Show the masks after the correction algorithm
